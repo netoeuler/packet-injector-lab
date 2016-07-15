07040c0a985f36b48b9dd1b5f7be3faaa0db5abd
Euler Neto <eulerneto.ngt@gmail.com>
2016-07-14 23:24:13 -0300

    create IP header

--- packet_injection.c
+++ packet_injection.c
@@ -12,6 +12,8 @@ Based on code from this link: http://hackoftheday.securitytube.net/2013/04/my-co
 #include <sys/ioctl.h>
 #include <net/if.h>
 #include <net/ethernet.h>
+#include <linux/ip.h>
+#include <arpa/inet.h> //IPPROTO_TCP
 
 #include <string.h> /* memset */
 #include <unistd.h> /* close */
@@ -25,12 +27,10 @@ Based on code from this link: http://hackoftheday.securitytube.net/2013/04/my-co
 #define DST_IP	"192.168.0.11"
 
 
-int CreateRawSocket(int protocol_to_sniff)
-{
+int CreateRawSocket(int protocol_to_sniff){
 	int rawsock;
 
-	if((rawsock = socket(PF_PACKET, SOCK_RAW, htons(protocol_to_sniff)))== -1)
-	{
+	if((rawsock = socket(PF_PACKET, SOCK_RAW, htons(protocol_to_sniff)))== -1)	{
 		perror("Error creating raw socket: ");
 		exit(-1);
 	}
@@ -38,8 +38,7 @@ int CreateRawSocket(int protocol_to_sniff)
 	return rawsock;
 }
 
-int BindRawSocketToInterface(char *device, int rawsock, int protocol)
-{
+int BindRawSocketToInterface(char *device, int rawsock, int protocol){
 	
 	struct sockaddr_ll sll;
 	struct ifreq ifr;
@@ -49,8 +48,7 @@ int BindRawSocketToInterface(char *device, int rawsock, int protocol)
 	
 	/* First Get the Interface Index  */
 	strncpy((char *)ifr.ifr_name, device, IFNAMSIZ);
-	if((ioctl(rawsock, SIOCGIFINDEX, &ifr)) == -1)
-	{
+	if((ioctl(rawsock, SIOCGIFINDEX, &ifr)) == -1)	{
 		printf("Error getting Interface index !\n");
 		exit(-1);
 	}
@@ -61,8 +59,7 @@ int BindRawSocketToInterface(char *device, int rawsock, int protocol)
 	sll.sll_protocol = htons(protocol); 
 
 
-	if((bind(rawsock, (struct sockaddr *)&sll, sizeof(sll)))== -1)
-	{
+	if((bind(rawsock, (struct sockaddr *)&sll, sizeof(sll)))== -1)	{
 		perror("Error binding raw socket to interface\n");
 		exit(-1);
 	}
@@ -72,21 +69,41 @@ int BindRawSocketToInterface(char *device, int rawsock, int protocol)
 }
 
 
-int SendRawPacket(int rawsock, unsigned char *pkt, int pkt_len)
-{
+int SendRawPacket(int rawsock, unsigned char *pkt, int pkt_len){
 	int sent= 0;
 
 	/* A simple write on the socket ..thats all it takes ! */
-
-	if((sent = write(rawsock, pkt, pkt_len)) != pkt_len)
-	{
+	if((sent = write(rawsock, pkt, pkt_len)) != pkt_len){ 
 		return 0;
 	}
 
 	return 1;
 }
 
-/*unsigned char* createIPHeader(){
+/* Ripped from Richard Stevans Book */
+unsigned short ComputeIpChecksum(unsigned char *header, int len){
+	long sum = 0;  /* assume 32 bit long, 16 bit short */
+ 	unsigned short *ip_header = (unsigned short *)header;
+
+     while(len > 1){
+         //sum += *((unsigned short*) ip_header)++;
+     	sum += *((unsigned short*) ip_header);
+     	ip_header++;
+         if(sum & 0x80000000)   /* if high order bit set, fold */
+         	sum = (sum & 0xFFFF) + (sum >> 16);
+         len -= 2;
+     }
+
+     if(len)       /* take care of left over byte */
+         sum += (unsigned short) *(unsigned char *)ip_header;
+      
+     while(sum>>16)
+         sum = (sum & 0xFFFF) + (sum >> 16);
+
+    return ~sum;
+}
+
+unsigned char* createIPHeader(){
 	struct iphdr *ip_header;
 
 	ip_header = (struct iphdr *)malloc(sizeof(struct iphdr));
@@ -99,16 +116,15 @@ int SendRawPacket(int rawsock, unsigned char *pkt, int pkt_len)
 	ip_header->frag_off = 0;
 	ip_header->ttl = 111;
 	ip_header->protocol = IPPROTO_TCP;
-	ip_header->check = 0;
-	(in_addr_t)ip_header->saddr = inet_addr(SRC_IP);
-	(in_addr_t)ip_header->daddr = inet_addr(DST_IP);
-
+	//ip_header->check = 0;
+	ip_header->saddr = inet_addr(SRC_IP);
+	ip_header->daddr = inet_addr(DST_IP);
 
 	// Calculate the IP checksum now : The IP Checksum is only over the IP header
-	//ip_header->check = ComputeIpChecksum((unsigned char *)ip_header, ip_header->ihl*4);
+	ip_header->check = ComputeIpChecksum((unsigned char *)ip_header, ip_header->ihl*4);
 
 	return ((unsigned char *)ip_header);
-}*/
+}
 
 unsigned char* createEthernetHeader(){
 	struct ethhdr *ethernet_header;
@@ -126,14 +142,12 @@ unsigned char* createEthernetHeader(){
 
 /* argv[1] is the device e.g. eth0
    argv[2] is the number of packets to send
-*/
- 
-main(int argc, char **argv)
-{
-
+*/ 
+int main(int argc, char **argv){
 	int raw;
-	//unsigned char packet[PACKET_LENGTH];
-	unsigned char* packet;
+	unsigned char *ethernet_header;
+	unsigned char *ip_header;
+	unsigned char* packet;	
 	int num_of_pkts;
 	int pkt_len;
 	
@@ -142,16 +156,19 @@ main(int argc, char **argv)
 
 	/* Bind raw socket to interface */
 	BindRawSocketToInterface(argv[1], raw, ETH_P_ALL);
-
-	packet = createEthernetHeader();
-	pkt_len = PACKET_LENGTH;
-	//pkt_len = sizeof(struct ethhdr);
-
 	num_of_pkts = atoi(argv[2]);
 
-	while((num_of_pkts--)>0)
-	{
+	ethernet_header = createEthernetHeader();
+	ip_header = createIPHeader();	
 
+	//pkt_len = PACKET_LENGTH;
+	pkt_len = sizeof(struct ethhdr) + sizeof(struct iphdr);
+
+	packet = (unsigned char *) malloc(pkt_len);
+	memcpy(packet, ethernet_header, sizeof(struct ethhdr));
+	memcpy((packet + sizeof(struct ethhdr)), ip_header, sizeof(struct iphdr));
+
+	while((num_of_pkts--)>0){
 		if(!SendRawPacket(raw, packet, pkt_len))
 			perror("Error sending packet");
 		else
